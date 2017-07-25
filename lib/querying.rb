624e864c0d99083d02dd11cbdbe620a1c1f3b7a8
Joshua Lacey <williamjoshualacey@gmail.com>
2017-07-25 09:33:44 -0400

    Update querying.rb

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,11 +24,12 @@ ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT  series.title  FROM  series
-JOIN characters
+  "SELECT series.title FROM  characters
+JOIN series
 ON series.id = characters.series_id
-GROUP BY series.title
-HAVING characters.species = \"human\""
+WHERE species = \"human\"
+GROUP BY characters.series_id
+ORDER BY COUNT(characters.id) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
